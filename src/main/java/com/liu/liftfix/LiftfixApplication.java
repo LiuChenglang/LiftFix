bf566b6e51ae813bd2f9d65b0383494a7664454e
Lang <631554746@qq.com>
2019-12-05 16:19:39 +0800

    init repo

---
+++ src/main/java/com/liu/liftfix/LiftfixApplication.java
@@ -0,0 +1,13 @@
+package com.liu.liftfix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiftfixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiftfixApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/liu/liftfix/LiftfixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liu.liftfix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiftfixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
